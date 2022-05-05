45ab6a7129f42877af9f9e02d9ec19f22c04cba2
Alexander Hernandez <alexaandergh@hotmail.com>
2022-05-05 18:12:25 -0500

    Agregando primer dato del example9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,19 @@
+class Pokemon {
+    constructor(name) {
+      this.name = name 
+    }
+  
+
+    info (message) {
+      console.log(`Mi pokemon: ${this.name}`)
+    }
+  
+ 
+  }
+  
+  module.exports = Pokemon
+
+  module.exports = (message) => {
+    console.log(`info: ${message}`)
+  }
+  
